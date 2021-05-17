22bdd75121b5a306f1a6aa2efd9464283616352c
omprakashalle <alleaum@gmail.com>
2021-05-17 13:01:00 +0400

    Updates

---
+++ precache-manifest.687133802a9d4b034a3dbfe998e46d0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a420b90b8e5733b4999e",
+    "url": "/omprakashalle/robofriends/static/css/main.7b36bf53.chunk.css"
+  },
+  {
+    "revision": "a420b90b8e5733b4999e",
+    "url": "/omprakashalle/robofriends/static/js/main.561d7887.chunk.js"
+  },
+  {
+    "revision": "adabe4e196c7ce7a80c4",
+    "url": "/omprakashalle/robofriends/static/js/runtime~main.efae1728.js"
+  },
+  {
+    "revision": "0d48b4aee2220c68683d",
+    "url": "/omprakashalle/robofriends/static/css/2.2c6ce6f1.chunk.css"
+  },
+  {
+    "revision": "0d48b4aee2220c68683d",
+    "url": "/omprakashalle/robofriends/static/js/2.a8ccf58a.chunk.js"
+  },
+  {
+    "revision": "f2c121df5bc05f03f3ea",
+    "url": "/omprakashalle/robofriends/static/js/3.b1248334.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/omprakashalle/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "3bf6b0dec1087b1d23f03967bcfba231",
+    "url": "/omprakashalle/robofriends/index.html"
+  }
+];
